959474a1b341f1c940a22bf06003ad29e48b5d7e
Nikolay <koliamba_5@mail.ru>
2023-10-25 19:45:12 +0800

    Task1 comlete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task1(){
+        int i=700;
+        byte b=125;
+        short s=258;
+        long l=555L;
+        float f=20.22f;
+        double d=40.88;
+
+        System.out.println("Значение переменной i с типом int равно "+i);
+        System.out.println("Значение переменной b с типом byte равно "+b);
+        System.out.println("Значение переменной s с типом short равно "+s);
+        System.out.println("Значение переменной l с типом long равно "+l);
+        System.out.println("Значение переменной f с типом float равно "+f);
+        System.out.println("Значение переменной d с типом double равно "+d);
     }
 }
